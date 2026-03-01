1f55bf60e48911e261b3cbb63e7f5201b4f26705
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-03-01 15:54:59 -0300

    Agrega una id a la lista de beneficios de Meli Plus

--- src/fakeDB/meliPlus.js
+++ src/fakeDB/meliPlus.js
@@ -4,14 +4,17 @@ export const meliPlusList = {
     },
     benefits: [
         {
+            id: 1,
             image: 'https://http2.mlstatic.com/resources/frontend/statics/loyal/partners/melimas/widget_home/benefits/box_desktop@2x.png',
             text: 'Envíos gratis en productos desde $ 16.000'
         },
         {
+            id: 2,
             image: 'https://http2.mlstatic.com/resources/frontend/statics/loyal/partners/melimas/widget_home/benefits/mla_cuotas_installments_desktop@2x.png',
             text: 'Hasta 3 cuotas extra en tus compras'
         },
         {
+            id: 3,
             image: 'https://http2.mlstatic.com/resources/frontend/statics/loyal/partners/partners/melimas/widget_home/benefits/delivery-desktop@2x.png',
             text: 'Envíos gratis en pedidos de “Restaurantes”'
         }
